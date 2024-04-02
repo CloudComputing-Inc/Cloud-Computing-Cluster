39c28400166b0b36233cf4a7fb857b91901c39e7
9-whiterose-9 <beatrizarosa4@gmail.com>
2024-04-02 15:30:59 +0000

    API gateway implemented

---
+++ api/app.py
@@ -0,0 +1,28 @@
+""" 
+from flask import Flask, request, jsonify
+from api.metadata_routes import metadata_bp
+
+app = Flask(__name__)
+app.register_blueprint(metadata_bp)
+
+@app.route('/')
+def default_route():
+    return "Welcome to the Market Performance API!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
+
+# 3rd party modules
+import connexion
+from flask_cors import CORS
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
+
+# read the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
+CORS(app.app)
+
+if __name__ == "__main__":
+    app.run()
--- api/metadata_routes.py
+++ api/metadata_routes.py
@@ -54,7 +54,125 @@ def get_cluster_connection_string(category):
         # Default or error handling
         return None
     
+def get_metadata(category, brand, min_price, max_price):
+    cluster_connection_string = get_cluster_connection_string(category)
+    if not cluster_connection_string:
+        return {"error": "Category not found or not supported."}, 404
+
+    client = MongoClient(cluster_connection_string)
+    db = client['amazon_metadata']  
+    collection = db['metadata']
     
+    query = {"category": category}
+    if brand:
+        query['brand'] = brand
+    if min_price is not None or max_price is not None:
+        query['price'] = {}
+        if min_price is not None:
+            query['price']['$gte'] = min_price
+        if max_price is not None:
+            query['price']['$lte'] = max_price
+
+    documents = collection.find(query)
+
+    response = [doc for doc in documents]
+    for doc in response:
+        doc.pop('_id', None)  # Remove MongoDB's '_id'
+
+    return jsonify(response)
+
+def get_main_categories():
+    main_categories = set()  # Use a set to avoid duplicates
+
+    for cluster_connection_string in clusters.values():
+        client = MongoClient(cluster_connection_string, tlsCAFile=certifi.where())
+        db = client['amazon_metadata']
+        collection = db['metadata']
+
+        # Modified pipeline to account for the 'category' field and filter out nulls
+        pipeline = [
+            {
+                "$project": {
+                    "main_cat_adjusted": {
+                        "$cond": {
+                            "if": {"$or": [
+                                {"$and": [
+                                    {"$isArray": "$category"},
+                                    {"$ne": [{"$arrayElemAt": ["$category", 0]}, "$main_cat"]}
+                                ]},
+                                {"$and": [
+                                    {"$not": {"$isArray": "$category"}},
+                                    {"$ne": ["$category", "$main_cat"]}
+                                ]}
+                            ]},
+                            "then": {"$cond": {"if": {"$isArray": "$category"}, "then": {"$arrayElemAt": ["$category", 0]}, "else": "$category"}},
+                            "else": "$main_cat"
+                        }
+                    }
+                }
+            },
+            {"$match": {"main_cat_adjusted": {"$ne": None}}},  # Filter out documents where main_cat_adjusted is null
+            {"$group": {"_id": "$main_cat_adjusted"}}
+        ]
+
+        categories = collection.aggregate(pipeline)
+        for category in categories:
+            main_categories.add(category["_id"])
+
+    return jsonify({"main_categories": list(main_categories)})
+
+def get_top_products():
+    category = request.args.get('category')
+    client = MongoClient(get_cluster_connection_string(category), tlsCAFile=certifi.where())
+    db = client['amazon_metadata']
+    collection = db['metadata']
+
+    # We define the projection here. '1' means to include the field, '_id' is set to 0 to exclude it.
+    projection = {'_id': 0, 'title': 1, 'brand': 1, 'price': 1}
+
+    # Add the projection to the find method call
+    top_products = collection.find({"main_cat": category}, projection).sort("rank", 1).limit(2)
+
+    # The response now only includes the specified fields
+    response = [doc for doc in top_products]
+
+    return jsonify(response)
+
+
+
+def get_top_brand():
+    category = request.args.get('category')
+    cluster_connection_string = get_cluster_connection_string(category)
+    if not cluster_connection_string:
+        return jsonify({"error": "Category not found or not supported."}), 404
+
+    client = MongoClient(cluster_connection_string, tlsCAFile=certifi.where())
+    db = client['amazon_metadata']
+    collection = db['metadata']
+
+    # Adjusted MongoDB aggregation to find the brand with the most products, ensuring the brand is not null or empty
+    top_brand = collection.aggregate([
+        {
+            "$match": {
+                "main_cat": category,
+                "brand": {"$exists": True, "$ne": ""}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$brand",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 1}
+    ])
+    top_brand_list = list(top_brand)
+    if top_brand_list and top_brand_list[0]['_id']:
+        return jsonify({"top_brand": top_brand_list[0]['_id'], "count": top_brand_list[0]['count']})
+    return jsonify({"error": "No data found for the category."})
+
+""" 
 metadata_bp = Blueprint('metadata', __name__, url_prefix='/api/v1')
 
 @metadata_bp.route('/metadata', methods=['GET'])
@@ -189,6 +307,7 @@ def get_top_brand():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
+"""
 
 
