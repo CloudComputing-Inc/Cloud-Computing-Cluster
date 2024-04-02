1ec2a1099ede04b55b140923d0957ec391f2d20a
9-whiterose-9 <beatrizarosa4@gmail.com>
2024-04-02 22:50:30 +0000

    added welcome msg

--- api/app.py
+++ api/app.py
@@ -1,28 +1,30 @@
-""" 
-from flask import Flask, request, jsonify
-from api.metadata_routes import metadata_bp
-
-app = Flask(__name__)
-app.register_blueprint(metadata_bp)
-
-@app.route('/')
-def default_route():
-    return "Welcome to the Market Performance API!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
-# 3rd party modules
 import connexion
 from flask_cors import CORS
+from flask import jsonify
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir="./")
 
 # read the swagger.yml file to configure the endpoints
 app.add_api("swagger.yml")
+
+# Enable CORS
 CORS(app.app)
+
+# Define a route for the root URL
+@app.app.route('/')
+def welcome():
+    response = {
+        "Welcome": "Welcome to the Amazon Reviews Analyzer API!",
+        "Note": "Please refer to the Swagger UI for more detailed information on each endpoint.",
+        "Endpoints": {
+            "/api/v1/categories": "Retrieve a list of product categories.",
+            "/api/v1/metadata": "Fetch metadata for products with optional filtering.",
+            "/api/v1/top-brand": "Get the top brand in a specified category.",
+            "/api/v1/top-products": "Retrieve top products in a category."
+        }
+    }
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 ## Populating databases and preprocessing data........
+printf "Populating databases and preprocessing data"
 #cd ~/cn-group03/app/data/Amazon_metadata/
 #python metadata_clusters.py
 
 ## Creating Protobuf python files..........
+printf "Creating Protobuf python files"
 cd ~/cn-group03/scripts
 chmod +x ~/cn-group03/scripts/proto_create.sh
 ./proto_create.sh
 
-#cd ~/cn-group03/api/
-#python app.py
-
 ## Building and Running docker containers........
+printf "Building and Running docker containers"
 docker-compose up --build
