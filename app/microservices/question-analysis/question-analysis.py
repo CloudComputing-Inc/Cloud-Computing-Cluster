5f3c73b07285b5347fe3eb2be6eadfa485592473
fc63327 <fc63327@alunos.ciencias.ulisboa.pt>
2024-04-10 02:03:01 +0200

    qa initialize question-analysis.py

--- app/microservices/question-analysis/question-analysis.py
+++ app/microservices/question-analysis/question-analysis.py
@@ -3,3 +3,13 @@ import question_analysis_pb2
 import question_analysis_pb2_grcp
 from pymongo import MongoClient
 from concurrent import futures
+
+# connect to MongoDB, change the user and passwordX  to reflect your own clusters  connection strings
+user = "need_user"
+password = "needpassword"
+up = user + ":" + password
+
+client = MongoClient("mongodb+srv://"+up+"@qna.lnpq3.mongodb.net/Qna?retryWrites=true&w=majority")
+db = client["database"]
+db = db["qna"]
+
