3bf5c6df4e5963c16b2b1b1130e187984ed3d696
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-04-20 18:51:29 +0100

    add permission

--- scripts/gcp_configure.sh
+++ scripts/gcp_configure.sh
@@ -1,5 +1,9 @@
-gcloud iam service-accounts create gcloud-project-acc --description="Account for local deployment of k8s" --display-name="Local Account"
+gcloud iam service-accounts create gcloud-project-acc \
+    --description="Account for local deployment of k8s" \
+    --display-name="Local Account" \
+gcloud projects add-iam-policy-binding cloudcomputinginc    --member=serviceAccount:gcloud-project-acc@cloudcomputinginc.iam.gserviceaccount.co
+m    --role=roles/artifactregistry.admin
 gcloud iam service-accounts list
 gcloud iam service-accounts keys create ./gcr.json --iam-account gcloud-project-acc@cloudcomputinginc.iam.gserviceaccount.com
-kubectl create secret docker-registry gcr-io --docker-server=gcr.io --docker-username=_json_key --docker-password="$(cat ./gcr.json)" --docker-email=any@valid.email
+kubectl create secret docker-registry gcr-io --docker-server=gcr.io --docker-username=_json_key --docker-password="$(cat ./gcr.json)" --docker-email=any@example.com
 kubectl get secrets
