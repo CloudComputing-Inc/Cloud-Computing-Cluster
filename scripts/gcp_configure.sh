64c256d81d5b491636d92643d5fc861f4c8937cd
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-04-21 01:11:28 +0100

    ok

--- scripts/gcp_configure.sh
+++ scripts/gcp_configure.sh
@@ -1,3 +1,6 @@
+##########################################
+# Already done once, no need to run again#
+##########################################
 gcloud iam service-accounts create gcloud-project-acc \
     --description="Account for local deployment of k8s" \
     --display-name="Local Account" \
@@ -5,5 +8,8 @@ gcloud projects add-iam-policy-binding cloudcomputinginc    --member=serviceAcco
 m    --role=roles/artifactregistry.admin
 gcloud iam service-accounts list
 gcloud iam service-accounts keys create ./gcr.json --iam-account gcloud-project-acc@cloudcomputinginc.iam.gserviceaccount.com
+################################################################################
+# From here, we can use the gcr.json file to authenticate with the GCP registry#
+################################################################################
 kubectl create secret docker-registry gcr-io --docker-server=gcr.io --docker-username=_json_key --docker-password="$(cat ./gcr.json)" --docker-email=any@example.com
 kubectl get secrets
