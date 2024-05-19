0d372c3eeb121365d0299a7450a9812d842cb7a4
9-whiterose-9 <beatrizarosa4@gmail.com>
2024-05-19 00:37:44 +0000

    grafana svc and deployment added and running

--- scripts/gcr_apply_kubernetes.sh
+++ scripts/gcr_apply_kubernetes.sh
@@ -7,3 +7,4 @@ PROMETHEUS_DIR=~/cn-group03/prometheus
 kubectl apply -f $PROMETHEUS_DIR/prometheus-config.yaml
 kubectl apply -f $PROMETHEUS_DIR/prometheus-deployment.yaml
 kubectl apply -f $PROMETHEUS_DIR/prometheus-service.yaml
+kubectl apply -f $PROMETHEUS_DIR/grafana.yaml
