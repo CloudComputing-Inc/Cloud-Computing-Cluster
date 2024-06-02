e42e678cbf150a55b0cb00bb0368aacf15085c44
9-whiterose-9 <beatrizarosa4@gmail.com>
2024-06-02 18:52:47 +0000

    locust now gets external ip dynamically

--- scripts/gcr_apply_kubernetes.sh
+++ scripts/gcr_apply_kubernetes.sh
@@ -1,4 +1,4 @@
-# Applies Kubernetes configurations for your microservices
+# Applies Kubernetes configurations for microservices
 for file in kubernetes/*/*.yaml; do kubectl apply -f "$file"; done
 
 # Apply Prometheus configurations
@@ -10,6 +10,26 @@ kubectl apply -f $PROMETHEUS_DIR/prometheus-service.yaml
 kubectl apply -f $PROMETHEUS_DIR/grafana.yaml
 
 # Apply Locust configuration
-kubectl create configmap locust-scripts --from-file=kubernetes/locust/locust_marketAnalysis.py
+kubectl create configmap locust-scripts --from-file=kubernetes/locust/locust_marketAnalysis.py || kubectl create configmap locust-scripts --from-file=kubernetes/locust/locust_marketAnalysis.py --dry-run=client -o yaml | kubectl apply -f -
 kubectl get configmap locust-scripts
-kubectl apply -f kubernetes/locust/locust-deployment.yaml
+
+# Fetch the external IP of nginx-gateway-svc
+EXTERNAL_IP=""
+while [ -z "$EXTERNAL_IP" ]; do
+  echo "Waiting for external IP of nginx-gateway-svc..."
+  EXTERNAL_IP=$(kubectl get svc nginx-gateway-svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  [ -z "$EXTERNAL_IP" ] && sleep 10
+done
+
+echo "External IP found: $EXTERNAL_IP"
+
+# Path to the locust deployment yaml file
+LOCUST_DEPLOYMENT_YAML="kubernetes/locust/locust-deployment.yaml"
+
+# Update the locust-deployment.yaml with the fetched EXTERNAL_IP
+sed -i "s|--host=http://PLACEHOLDER|--host=http://$EXTERNAL_IP|g" $LOCUST_DEPLOYMENT_YAML
+
+echo "Updated locust-deployment.yaml with external IP: $EXTERNAL_IP"
+
+# Apply the updated locust-deployment.yaml
+kubectl apply -f $LOCUST_DEPLOYMENT_YAML
