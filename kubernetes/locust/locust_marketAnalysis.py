cad9fe64d9f47dc1c0fc6af9eac1947f059a2561
Ricardo <86163146+0xjrr@users.noreply.github.com>
2024-06-20 20:49:06 +0100

    chore: Add Yelp tasks for market analysis

--- kubernetes/locust/locust_marketAnalysis.py
+++ kubernetes/locust/locust_marketAnalysis.py
@@ -1,7 +1,6 @@
 from locust import HttpUser, TaskSet, task, between
 
 class MarketAnalysisTasks(TaskSet):
-
     @task(4)
     def get_categories(self):
         self.client.get("/market/categories")
@@ -18,6 +17,27 @@ class MarketAnalysisTasks(TaskSet):
     def get_top_products(self):
         self.client.get("/market/top-products?category=Books")
 
+class YelpTasks(TaskSet):
+    @task(3)
+    def list_businesses(self):
+        self.client.get("/yelp/businesses?page=1&limit=20")
+
+    @task(3)
+    def get_top_rated_businesses(self):
+        self.client.get("/yelp/businesses/top-rated?city=San Francisco&state=CA&limit=10")
+
+    @task(2)
+    def get_business(self):
+        self.client.get("/yelp/businesses/sample_business_id")
+
+    @task(1)
+    def list_cities(self):
+        self.client.get("/yelp/cities")
+
+    @task(1)
+    def list_states(self):
+        self.client.get("/yelp/states")
+
 class MarketAnalysisUser(HttpUser):
-    tasks = [MarketAnalysisTasks]
+    tasks = [MarketAnalysisTasks, YelpTasks]
     wait_time = between(1, 5)
